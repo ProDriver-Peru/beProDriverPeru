e1753aaadf3c14ad884f18b5888c6ce2d05b8ad1
Cristhian Laines Gonzales <u20201c164@upc.edu.pe>
2023-06-17 08:38:47 -0500

    commit15
    Co-Authored-By: Desire Valenzuela <90126213+AlexValen9@users.noreply.github.com>
    Co-Authored-By: Piero Humberto Salinas Llorca <94383241+Pierochess@users.noreply.github.com>
    Co-Authored-By: VeDoble9719 <132007426+VeDoble9719@users.noreply.github.com>
    Co-Authored-By: Fernando Schilder <94210083+fernandoschilder@users.noreply.github.com>
    Co-Authored-By: TonyStark-13 <81401239+TonyStark-13@users.noreply.github.com>

--- src/main/java/pe/prodriverperu/beprodriverperu/repositories/RepositoryJobOffer.java
+++ src/main/java/pe/prodriverperu/beprodriverperu/repositories/RepositoryJobOffer.java
@@ -6,7 +6,7 @@ import pe.prodriverperu.beprodriverperu.entities.Joboffer;
 import java.util.List;
 
 public interface RepositoryJobOffer extends JpaRepository<Joboffer,Integer> {
-    List<Joboffer> findByIdEmployer(Integer idEmployer);
+    List<Joboffer> findByIdEmployerEquals(Integer idEmployer);
         /*
     @Query("SELECT jo FROM Joboffer jo WHERE jo.idEmployer=:pk")
     List<Joboffer> findEmployer(@Param("pk") Integer pk);
